3d502df158155ac50696775b0e4bb3eab33777d5
BadPioche <dwillemain@hotmail.fr>
2023-11-22 10:27:22 +0100

    db commit

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  '', // TODO: database connection string
+  "postgres://cloud:IH7L0KPyPS5vNLCONtfbMPmB3D8STo3g@dpg-clesd9uf27hc73bg2chg-a/cloud_g9n6", // TODO: database connection string
   {
     dialect: 'postgres',
     dialectOptions: {
