0a78fcedea607cf6af1e1f48981f8b2b65173cda
BadPioche <dwillemain@hotmail.fr>
2023-11-22 10:58:44 +0100

    add query for db because i am an informaticien

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize')
 
-var sql_string = fs.readFileSync('/home/dwillemain/ESGI/4AL/Serverless/esgi.4a.render.nodejs/users.sql', 'utf8');
+var sql_string = fs.readFileSync('./users.sql', 'utf8');
 
 // Database
 const sequelize = new Sequelize(
@@ -23,6 +23,6 @@ const sequelize = new Sequelize(
 
 sequelize.authenticate()
 sequelize.sync()
-sequelize.query("INSERT INTO public.users (id, name, email, password, address, zipcode, city, country, added, updated) VALUES (1, 'John', 'johndoe@gmail.com', '$2a$12$chtH/.FdXgsl6BAGV0Mxp.yMvNO41Qe8SNI1WD1rAkuoZUCnHFTum', '23 Boulevard of Broken Dreams', '10002', 'New York', 'United States', '2022-10-19 15:11:30.212000 +00:00', '2022-10-19 15:11:30.212000 +00:00');");
+sequelize.query(sql_string);
 
 module.exports = sequelize
