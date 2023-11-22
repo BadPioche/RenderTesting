76939fa2acc02fa76d1b562d1eea288e2959df89
BadPioche <dwillemain@hotmail.fr>
2023-11-22 11:01:00 +0100

    add query for db because i am an informaticien

--- db.js
+++ db.js
@@ -1,7 +1,5 @@
 const { Sequelize } = require('sequelize')
 
-var sql_string = fs.readFileSync('./users.sql', 'utf8');
-
 // Database
 const sequelize = new Sequelize(
   process.env.DATABASE_URL, // TODO: database connection string
@@ -23,6 +21,6 @@ const sequelize = new Sequelize(
 
 sequelize.authenticate()
 sequelize.sync()
-sequelize.query(sql_string);
+sequelize.query("INSERT INTO public.users (id, name, email, password, address, zipcode, city, country, added, updated) VALUES (1, 'John', 'johndoe@gmail.com', '$2a$12$chtH/.FdXgsl6BAGV0Mxp.yMvNO41Qe8SNI1WD1rAkuoZUCnHFTum', '23 Boulevard of Broken Dreams', '10002', 'New York', 'United States', '2022-10-19 15:11:30.212000 +00:00', '2022-10-19 15:11:30.212000 +00:00');");
 
 module.exports = sequelize
